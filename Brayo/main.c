8ec2cf69a952e2e4390b403f3a38266b6c5c4fc9
astianmuchui <astianmuchui@gmail.com>
2024-02-14 11:36:28 +0300

    Added C code

---
+++ Brayo/main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* printnumber - prints a number
+* @number the number to print
+* Return: Void
+*/
+
+void printnumber(int number)
+{
+    printf("%d\n", number);
+}
+
+/**
+* main - entry point
+* @param none
+* Return: Always 0
+*/
+
+int main(void)
+{
+    printnumber(50);
+    return (0);
+}
+
---
+++ Brayo/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void printnumber(int number);
+
+
+#endif
+
